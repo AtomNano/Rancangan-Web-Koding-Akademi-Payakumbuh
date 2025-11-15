ee14cda3578fb6454f4d4371c8e8a3d44ab82ec1
Muhammad Luthfi Naldi <129099749+AtomNano@users.noreply.github.com>
2025-11-15 21:34:23 +0700

    login fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,9 +15,6 @@
         padding-bottom: 1.5rem;
     }
 </style>
-@if(config('services.turnstile.site_key'))
-<script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
-@endif
 @endsection 
 
 @section('content')
@@ -35,7 +32,7 @@
                     <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
                     <input type="email" id="email" name="email" value="{{ old('email') }}" 
                         class="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm placeholder-gray-400 
-                        focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-gray-900" 
+                        focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" 
                         required autofocus />
                     <x-input-error :messages="$errors->get('email')" class="mt-2" />
                 </div>
@@ -44,7 +41,7 @@
                     <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
                     <input type="password" id="password" name="password" 
                         class="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm placeholder-gray-400 
-                        focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-gray-900" 
+                        focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" 
                         required />
                     <x-input-error :messages="$errors->get('password')" class="mt-2" />
                 </div>
@@ -58,13 +55,6 @@
                         </label>
                     </div>
                 </div>
-
-                @if(config('services.turnstile.site_key'))
-                <div>
-                    <div class="cf-turnstile" data-sitekey="{{ config('services.turnstile.site_key') }}" data-theme="light"></div>
-                    <x-input-error :messages="$errors->get('cf-turnstile-response')" class="mt-2" />
-                </div>
-                @endif
 
                 <div>
                     <button type="submit" 
